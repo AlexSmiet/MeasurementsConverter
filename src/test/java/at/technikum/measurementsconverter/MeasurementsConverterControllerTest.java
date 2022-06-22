8b06ea1bdfa3aa34e8364ac9eef7b31c46984d00
smietana <alexander.smietana@ocg.at>
2022-06-22 16:48:34 +0200

    This commit will add the testing for stone to kilogram convertion

--- src/test/java/at/technikum/measurementsconverter/MeasurementsConverterControllerTest.java
+++ src/test/java/at/technikum/measurementsconverter/MeasurementsConverterControllerTest.java
@@ -8,40 +8,79 @@ class MeasurementsConverterControllerTest {
     MeasurementsConverterController MCC = new MeasurementsConverterController();
 
     @Test
-    void ounce_to_kilogram_test1(){
+    void ounce_to_gram_test1(){
         // Arrange
         double a = 1;
 
         double expected = 28.35;
 
         // Act
-        double result = MCC.ounce_to_kilogram(a);
+        double result = MCC.ounce_to_gram(a);
 
         // Assert
         assertEquals(expected, result);
     }
     @Test
-    void ounce_to_kilogram_test2(){
+    void ounce_to_gram_test2(){
         // Arrange
         double a = 0;
 
         double expected = 0;
 
         // Act
-        double result = MCC.ounce_to_kilogram(a);
+        double result = MCC.ounce_to_gram(a);
 
         // Assert
         assertEquals(expected, result);
     }
     @Test
-    void ounce_to_kilogram_test3(){
+    void ounce_to_gram_test3(){
         // Arrange
         double a = -1;
 
         double expected = -28.35;
 
         // Act
-        double result = MCC.ounce_to_kilogram(a);
+        double result = MCC.ounce_to_gram(a);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+    @Test
+    void stone_to_kilogram_test1(){
+        // Arrange
+        double a = 1;
+
+        double expected = 6.35;
+
+        // Act
+        double result = MCC.stone_to_kilogram(a);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+    @Test
+    void stone_to_kilogram_test2(){
+        // Arrange
+        double a = 0;
+
+        double expected = 0;
+
+        // Act
+        double result = MCC.stone_to_kilogram(a);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+    @Test
+    void stone_to_kilogram_test3(){
+        // Arrange
+        double a = -1;
+
+        double expected = -6.35;
+
+        // Act
+        double result = MCC.stone_to_kilogram(a);
 
         // Assert
         assertEquals(expected, result);
