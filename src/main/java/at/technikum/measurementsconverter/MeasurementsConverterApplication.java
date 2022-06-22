ab94a43d3658a5f74459cc3aa1192f00ecf1857d
smietana <alexander.smietana@ocg.at>
2022-06-22 15:34:16 +0200

    Initial commit

---
+++ src/main/java/at/technikum/measurementsconverter/MeasurementsConverterApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.measurementsconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementsConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasurementsConverterApplication.class, args);
+    }
+
+}
